211f8bd6c851b8014e59af5ba839e5cf77a15272
Dcvallejo <dcvallejo@unicauca.edu.co>
2024-04-16 10:49:29 -0500

    Homework

---
+++ M01_herramientaseinstalacion/Carpeta de. prueba/empiezo_en_Python.py
@@ -0,0 +1,23 @@
+print("Hola Grupo, Bienvenidos a Henry")
+
+"""paso 1- me paro de la cama.
+si me paro del lado izquierdo:
+    tengo ampliar el recorrido  
+        cruzar a la derecha 2 veces
+        si estan las chanclas:
+            ponermelas
+        sino buscarlas.
+sino: 
+    me pongo las chanclas"""
+
+snake_case = "soy snake case" # lo que mas se utiliza
+CamelCase = "Spy Camell Case"
+
+
+
+num = input("ingrese un numero : ")
+print(num)
+
+edad = 37
+print(type(edad))
+
